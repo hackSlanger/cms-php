577e48b63b7a22e9441fcc359c04c854a925f906
ghostfighter1 <riteshbhat17@gmail.com>
2017-03-22 18:10:58 +0530

    article details page added

---
+++ articleDetails.php
@@ -0,0 +1,20 @@
+<?php
+
+include 'dbh.php';
+session_start();
+
+$id = $_GET["id"];
+
+$sql="SELECT title,content,releaseDate from articles where id='$id'";
+$result = $conn->query($sql);
+if($result->num_rows > 0){
+	while ($row = $result->fetch_assoc()) {
+		echo "<b>TITLE:</b>".$row["title"]."<br>";
+		echo "<b>Publishing date:</b>".$row["releaseDate"]."<br>";
+		echo "<b>Content</b>".$row["content"];
+	}
+} else{
+	echo "Nothing to show , sorry try again";
+}
+
+?>
--- index.php
+++ index.php
@@ -7,13 +7,13 @@ session_start();
  } else{
  	header("location: login.php");
  }
- $sql = "SELECT title,description,releaseDate from articles";
+ $sql = "SELECT id,title,description,releaseDate from articles";
 
  $result = $conn->query($sql);
 
  if($result->num_rows >0){
  	while ($row = $result->fetch_assoc()) {
- 		echo "<b>".$row["title"]."</b><br>";
+ 		echo "<a href=articleDetails.php?id=".$row["id"]."> <b>".$row["title"]."</b></a><br>";
  		echo "<p>".$row["releaseDate"]."</p>";
  		echo "<p>".$row["description"]."</p>";
  	}
